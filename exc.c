3e9bd8442a42eeebb58db3422c0cd571c97e663c
Thiennnnlq <142667870+Thiennnnlq@users.noreply.github.com>
2024-09-17 12:11:14 +0000

    Add function

--- exc.c
+++ exc.c
@@ -8,11 +8,26 @@ int factorial_exponential(int n) {
     return n * factorial_exponential(n-1);
 }
 
+int add_num(int a, int b) {
+    if (n == 0) {
+        return 1;
+    }
+    return a + b;
+}
+
 main() {
     int n;
-    printf("Enter a number: ");
+    int a;
+    int b;
+    printf("Enter num1: ");
     scanf("%d", &n);
-    int result = factorial_exponential(n);
-    printf("Factorial of %d is %d\n", n, result);
+    printf("Enter num2: ");
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    int result1 = factorial_exponential(n);
+    int result2 = add_num(a, b)
+    printf("Factorial of %d is %d\n", n, result1);
+    printf("Sum of %d and %d is %d", a, b, result2)
     return 0;
 }
